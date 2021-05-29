4bf57259d927fb61b31b909d5febc7bd41841389
agostontamas <agoston.tamas@ursys.net>
2021-05-29 20:30:36 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticleComponent } from './article/article.component';
+import { ArticlesComponent } from './articles/articles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'articles', component: ArticlesComponent },
+  { path: 'articles/:id', component: ArticleComponent},
+  { path: '**', redirectTo: 'articles' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ArticlesComponent, ArticleComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,18 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ArticlesComponent } from './articles/articles.component';
 import { ArticleCardComponent } from './articles/article-card/article-card.component';
-import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ArticlesComponent,
+    routingComponents,
     ArticleCardComponent,
-    ArticleComponent,
   ],
   imports: [
     BrowserModule,
