717a7b21ee835fe602c94b17072777e276ddf4dc
Aron <arontheking@gmail.com>
2021-06-01 12:57:09 +0000

    update karma conf file

--- karma.conf.js
+++ karma.conf.js
@@ -8,9 +8,8 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('karma-mocha-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
